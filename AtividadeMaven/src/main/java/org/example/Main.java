b30142cfa50b2821ede4ed8cad8f7fa23f8f353f
Seu Nome <OKamizura@users.noreply.github.com>
2025-08-18 20:02:09 -0300

    Código-Fonte adicionado

---
+++ AtividadeMaven/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String conteudo = FileUtils.readFileToString(new File("mensagem.txt"), StandardCharsets.UTF_8);
+        System.out.println(conteudo);
+        FileUtils.writeStringToFile(new File("mensagem.txt"), "Oi Bia", StandardCharsets.UTF_8);
+    }
+}
