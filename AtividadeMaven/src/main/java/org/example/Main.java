f8cb49cd61d328b782af36087254efdb60ca9845
Seu Nome <OKamizura@users.noreply.github.com>
2025-08-18 20:48:37 -0300

    Oi Zeca

--- AtividadeMaven/src/main/java/org/example/Main.java
+++ AtividadeMaven/src/main/java/org/example/Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String conteudo = FileUtils.readFileToString(new File("mensagem.txt"), StandardCharsets.UTF_8);
         System.out.println(conteudo);
-        FileUtils.writeStringToFile(new File("mensagem.txt"), "Oi Bia", StandardCharsets.UTF_8);
+        FileUtils.writeStringToFile(new File("mensagem.txt"), "Oi Zeca", StandardCharsets.UTF_8);
     }
 }
